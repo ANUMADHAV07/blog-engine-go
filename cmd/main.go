12f78a9d03c75a5a044ccbb2e94bb3b7ec2da7f6
Abhi <abhiyos@ABHIs-MacBook-Air.local>
2025-07-14 16:14:23 +0530

    Feat : added chi router for http

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Blog-engine-go")
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+
+	err := http.ListenAndServe(":8080", r)
+
+	if err != nil {
+		// fmt.Printf("ERROR %v", err)
+		fmt.Println("ERROR", err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ANUMADHAV07/blog-engine-go.git
 
 go 1.24.3
+
+require github.com/go-chi/chi/v5 v5.2.2 // indirect
